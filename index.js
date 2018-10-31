5b4c04f2c5dbd782de41e83fb8a53d7f0497e486
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-10-31 22:45:15 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,27 +1,59 @@
+const path = require('path')
 const exec = require('child_process').exec
 const shell = require('shelljs')
-const current = __dirname
-const package = shell.find(__dirname)
-  .filter(item => !item.includes('node_modules'))
-  .filter(item => item.includes('package.json'))
-  .map(item => item.replace('package.json', ''))
 
-const cmd = 'yarn'
+const package = (opts) => {
+  const directory = path.resolve(
+    __dirname,
+    (opts && opts.root) || __dirname
+  )
 
-const install = (path) => {
+  return shell.find(directory)
+    .filter(item => !item.includes('node_modules'))
+    .filter(item => item.includes('package.json'))
+    .map(item => item.replace('package.json', ''))
+}
+
+const install = (manager, path) => {
   return new Promise((resolve, reject) => {
+    const command = (
+      manager === 'npm' ? 'npm install' : 'yarn'
+    )
+
     shell.cd(path)
 
-    exec(cmd, (err, out) => {
+    exec(command, (err, out) => {
       if (err) reject(err)
-      else resolve(out)
+      resolve(out)
     })
   })
 }
 
-Promise.all(package.map(item => {
-  return install(item)
-})).then(res => {
-  shell.cd(current)
-  console.log('All dependencies already installed')
-})
+const manager = (opts) => {
+  return opts && opts.manager || (
+    shell.which('yarn') ? 'yarn' :
+    shell.which('npm') ? 'npm' :
+    false
+  )
+}
+
+const recuint = (opts) => {
+  return new Promise(resolve => {
+    Promise.all(package(opts).map(item => {
+      return install(manager(opts), item)
+    })).then(() => {
+      resolve('All dependencies already installed')
+    })
+  })
+}
+
+const recuintSync = (opts) => {
+  Promise.all(package(opts).map(item => {
+    return install(manager(opts), item)
+  }))
+}
+
+module.exports = {
+  recuint,
+  recuintSync
+}
